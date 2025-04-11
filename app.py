92fc2695e479d529a92b165f8bf55ca106b24f9d
Syed <syed123khizar@gmail.com>
2025-04-12 03:43:13 +0500

    changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask import request, jsonify, g
 from azure.storage.blob import BlobServiceClient
 import uuid
 import pymongo
