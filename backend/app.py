e28481235ce3ea066f1472a2908b7edb8a6e8f99
Syed <syed123khizar@gmail.com>
2025-04-11 19:29:47 +0500

    port

--- backend/app.py
+++ backend/app.py
@@ -183,4 +183,5 @@ def rate(title):
     return jsonify({"message": "Rating added"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Azure sets PORT env var
+    app.run(host='0.0.0.0', port=port, debug=True)
